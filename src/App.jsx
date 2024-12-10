2e6e517d9ba5d2063b4da0eeb3372f000ab84c62
Samantha Tolentino <S.Tolentino0828@gmail.com>
2024-12-10 18:45:21 -0500

    Deployment fixes 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Router, Routes, Route } from 'react-router-dom'
 import axios from 'axios'
 
 import Home from './pages/Home'
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/fish-app/' element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   )
