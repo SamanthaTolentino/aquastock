5fd677c783346dbffd83781d9b835d3bbc5ea95b
Samantha Tolentino <S.Tolentino0828@gmail.com>
2024-12-10 17:37:40 -0500

    Changed route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/fish-app/' element={<Home />} />
       </Routes>
     </>
   )
