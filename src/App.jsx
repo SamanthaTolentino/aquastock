711d1a5ea1871546e31c038c45a80ea39c75a6aa
Samantha Tolentino <S.Tolentino0828@gmail.com>
2024-12-10 20:25:50 -0500

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/fish-app/' element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   )
